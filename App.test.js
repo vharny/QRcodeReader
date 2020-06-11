8bde87074ae9e166d47b990b48f0f536f40cd89c
catrx <antonin.catrix@epsi.fr>
2020-06-11 13:48:25 +0200

    setting up test with jest

---
+++ App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { cleanup, render } from 'react-native-testing-library';
+import App from './App'
+
+afterEach(cleanup)
+
+describe('<App />', () => {
+  it('has 2 child', () => {
+    const tree = render(<App />).toJSON();
+    console.log(tree)
+    expect(tree.children.length).toBe(2);
+  });
+});
